b791a449c0329587ca2a6eb56ae6c2bb0c5ea4e5
Dan Carlier <dan.carlier@gmail.com>
2022-09-09 15:02:05 +1000

    Added bulma css library and created NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template> 
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
-
+  <NavBar />
   <RouterView/>
 </template>
+
+<script setup>
+/*
+  imports
+*/
+
+import NavBar from '@/components/layout/NavBar.vue'
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+
+</style>
