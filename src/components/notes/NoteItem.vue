ef95a2a970e3d6c41a62c5206bbeaac34aa4520b
Dan Carlier <dan.carlier@gmail.com>
2022-09-13 11:46:25 +1000

    Show note character length

--- src/components/notes/NoteItem.vue
+++ src/components/notes/NoteItem.vue
@@ -4,6 +4,9 @@
     >
       <div class="card-content">
         <div class="content">{{ note.content }}</div>
+        <div class="has-text-right has-text-grey-light mt-2">
+          <small>{{ characterLength }}</small>
+        </div>
       </div>
       <footer class="card-footer">
         <a href="#" class="card-footer-item">Edit</a>
@@ -14,6 +17,12 @@
 
 <script setup>
 /*
+  imports
+*/
+
+import { computed } from 'vue'
+
+/*
   props
 */
 
@@ -22,5 +31,15 @@ const props = defineProps({
     type: Object,
     required: true
   }
+})
+
+/*
+  character length
+*/
+
+const characterLength = computed(() => {
+  let length = props.note.content.length
+  let description = length > 1 ? 'characters' : 'character'
+  return `${length} ${description}`
 })
 </script>
