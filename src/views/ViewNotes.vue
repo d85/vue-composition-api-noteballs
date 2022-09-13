a14c389eb159085a95f4fdf15510c5b4bab0a476
Dan Carlier <dan.carlier@gmail.com>
2022-09-13 11:34:39 +1000

    Extract NoteItem as a separate component

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -25,19 +25,12 @@
       </div>
     </div>
 
-    <div
+    <NoteItem
       v-for="note in notes"
       :key="note.id"
-      class="card mb-4"
+      :note="note"
     >
-      <div class="card-content">
-        <div class="content">{{ note.content }}</div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    </NoteItem>
   </div>
 </template>
 
@@ -47,6 +40,7 @@
 */
 
 import { ref } from 'vue'
+import NoteItem from '@/components/notes/NoteItem.vue'
 
 const newNote = ref('')
 const newNoteRef = ref(null)
