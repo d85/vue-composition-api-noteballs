c23fef882f31b1522357ad833b7bad0d26fb634a
Dan Carlier <dan.carlier@gmail.com>
2022-10-24 13:18:52 +1100

    Added firebase config (after checking that it's ok for the firebase apiKey to be included: 'https://firebase.google.com/docs/projects/api-keys')

---
+++ src/js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCxMVc18Q-kuifphYU1Rzv8S7BTrXSbv3Y',
+  authDomain: 'noteballs-39cae.firebaseapp.com',
+  projectId: 'noteballs-39cae',
+  storageBucket: 'noteballs-39cae.appspot.com',
+  messagingSenderId: '27441100534',
+  appId: '1:27441100534:web:fa0e4d72764d97954eded7'
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+  db
+}
