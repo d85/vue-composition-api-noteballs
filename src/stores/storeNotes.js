f829f930aaab4f6124846225dea7a004e18be374
Dan Carlier <dan.carlier@gmail.com>
2022-10-27 17:26:42 +1100

    Update note in Firestore DB

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { collection, onSnapshot, doc, setDoc, deleteDoc } from 'firebase/firestore'
+import { collection, onSnapshot, doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestore'
 import { db } from '@/js/firebase'
 
 const notesCollectionRef = collection(db, 'notes')
@@ -31,14 +31,14 @@ export const useStoreNotes = defineStore('storeNotes', {
       await setDoc(doc(notesCollectionRef, id), {
         content: newNoteContent
       })
-
     },
     async deleteNote(idToDelete) {
       await deleteDoc(doc(notesCollectionRef, idToDelete))
     },
-    updateNote(id, content) {
-      const index = this.notes.findIndex(note => note.id === id)
-      this.notes[index].content = content
+    async updateNote(id, content) {
+      await updateDoc(doc(notesCollectionRef, id), {
+        content
+      })
     }
   },
   getters: {
