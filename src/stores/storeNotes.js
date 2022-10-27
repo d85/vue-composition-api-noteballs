83440068c341bd60bf18dc2a7451fabe6641500a
Dan Carlier <dan.carlier@gmail.com>
2022-10-27 16:58:47 +1100

    Delete note from Firestore DB

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { collection, onSnapshot, doc, setDoc } from 'firebase/firestore'
+import { collection, onSnapshot, doc, setDoc, deleteDoc } from 'firebase/firestore'
 import { db } from '@/js/firebase'
 
 const notesCollectionRef = collection(db, 'notes')
@@ -33,8 +33,8 @@ export const useStoreNotes = defineStore('storeNotes', {
       })
 
     },
-    deleteNote(idToDelete) {
-      this.notes = this.notes.filter(note => note.id !== idToDelete)
+    async deleteNote(idToDelete) {
+      await deleteDoc(doc(notesCollectionRef, idToDelete))
     },
     updateNote(id, content) {
       const index = this.notes.findIndex(note => note.id === id)
